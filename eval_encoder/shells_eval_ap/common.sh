35a31301976a1b5ff039e53d8c3c38e9ff431ec8
Xiang An <anxiangsir@outlook.com>
2025-12-22 18:45:10 +0800

    updated

--- eval_encoder/shells_eval_ap/common.sh
+++ eval_encoder/shells_eval_ap/common.sh
@@ -10,7 +10,7 @@ export PYTHONPATH=../
 # Default dataset list (can be overridden in calling script)
 DEFAULT_DATASETS=(
     # "ssv2"
-    "diving48"
+    # "diving48"
     "perception_test"
     # "epic_verb"
     # "epic_noun"
